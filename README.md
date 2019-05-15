09e1b84c48dcd9f59a37e953f8abb6abc2fdde9b
Alexis <alexis.morin.mail@gmail.com>
2019-05-14 22:00:00 -0400

    unity mvp

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# Salon
-## A VR hair authoring tool.
+![header](images/header.png)
 
+While it *is* getting better, authoring hair cards for real-time engines is still pretty painful. With hair polycount in some games like FFXV running up to *15 thousand polygons*, even the less painful card authoring systems like xgen are still pretty tedious - It's kind of a shame that after having a bunch of zBrush fibermesh fun character artists have to live off of splines and slow-paced iteration.
 
-## Acknowledgements
+With this Unity project / Standalone tool authoring hair has never been easier - a little like Oculus' *Quill*, you simply point, drag and release to spawn strands of hair; you can then export the geometry to a third-party DCC for finetuning.
+
+### Install process
+
+Unlike most of my other stuff, this repo contains both a sample project and an executable release - It's up to you to decide if you want to do your grooming in-Unity (which can allow you to debug stuff you don't like or change materials) or use the more compact executable for portability (or if you use Godot or Unreal and don't have a Unity install on hand).
+
+![ui](images/ui.png)
+
+### Control Scheme
+
+Just launch the standalone app or the *Salon* Unity scene inside the project.
+
+- Motion Controller Trigger: Press and hold to draw hair.
+- Keyboard Backspace *or* Motion Controller Grip Button: Undo.
+- Spacebar: Export to OBJ. 
+
+![process](images/process.png)
+
+### Requirements
+- Unity 2019.2.0b1 or later
+- A VR headset with motion controllers (Tested on Oculus Rift CV1, ought to work with the HTC Vive + Wands and maybe with WMR headsets)
+
+### TODO
+- Make the multiple hair types backend actually work - a bunch of textures are in but it needs a bit of code.
+- Better in-headset UI and UX
+- Reference frame to load portraits
+
+### Acknowledgements
 [Hrafnkell Freyr Hlöðversson](https://wiki.unity3d.com/index.php/ObjExporter) for coming up with the OBJExporter script so I wouldn't have to.
 [Matt Dean, Adam Frisby and Alan Zucconi](https://github.com/AdamFrisby/UnityHairShader) for the packaged Hair shader (full MIT license in the package). 
+
+![output](images/output.png)
--- Salon/Assets/Runtime/salon.cs
+++ Salon/Assets/Runtime/salon.cs
@@ -6,10 +6,11 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class salon : MonoBehaviour {
+public class salon : MonoBehaviour
+{
 
     // hair card materials, selected from random
-    [Header ("Material Settings")]
+    [Header("Material Settings")]
     [SerializeField]
     Material[] shortHair;
     [SerializeField]
@@ -20,163 +21,312 @@ public class salon : MonoBehaviour {
     Material[] curlyHair;
     [SerializeField]
     Material[] flyawayHair;
-    [Space (10)]
+    [Space(10)]
 
-    [Header ("General Settings")]
+    [Header("General Settings")]
     public GameObject strandPrefab;
-    public GameObject playerHand;
-    public bool leftHanded = true;
+    public GameObject leftHand;
+    public GameObject rightHand;
     public Material outputMaterial;
+    public Camera exportCamera;
 
     // internals
-    List<TrailRenderer> hairStrands = new List<TrailRenderer> ();
-    List<Mesh> strandMeshes = new List<Mesh> ();
+    List<TrailRenderer> hairStrands = new List<TrailRenderer>();
+    List<Mesh> strandMeshes = new List<Mesh>();
     MeshFilter exportTarget;
     Material[] currentHairType;
-
     GameObject currentStrand;
     float triggerPress;
+    float grabPress;
     bool lastTriggering;
     bool triggering;
+    bool lastUndo;
+    bool undo;
 
-    void Start () {
-        VRSafetyCheck ();
-        exportTarget = GameObject.Find ("exportTarget").GetComponent<MeshFilter> ();
-        ChangeHairType (shortHair);
+    float triggerPressRight;
+    float grabPressRight;
+    bool lastTriggeringRight;
+    bool triggeringRight;
+    bool lastUndoRight;
+    bool undoRight;
+
+    void Start()
+    {
+        VRSafetyCheck();
+        exportTarget = GameObject.Find("exportTarget").GetComponent<MeshFilter>();
+        ChangeHairType(shortHair);
     }
 
-    void VRSafetyCheck () {
-        XRDevice.SetTrackingSpaceType (TrackingSpaceType.RoomScale);
+    void VRSafetyCheck()
+    {
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
     }
 
-    void Update () {
+    void Undo()
+    {
+        if (hairStrands.Count > 0)
+        {
+            Destroy(hairStrands[hairStrands.Count - 1].gameObject);
+            hairStrands.RemoveAt(hairStrands.Count - 1);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ExportOBJ();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Undo();
+        }
+
         lastTriggering = triggering;
+        lastUndo = undo;
+        lastTriggeringRight = triggeringRight;
+        lastUndoRight = undoRight;
 
-        List<XRNodeState> xrNodes = new List<XRNodeState> ();
-        InputTracking.GetNodeStates (xrNodes);
+        List<XRNodeState> xrNodes = new List<XRNodeState>();
+        InputTracking.GetNodeStates(xrNodes);
 
-        for (int i = 0; i < xrNodes.Count; i++) {
+        for (int i = 0; i < xrNodes.Count; i++)
+        {
 
-            if (leftHanded) {
-                if (xrNodes[i].nodeType == XRNode.LeftHand) {
-                    Vector3 newHandPosition;
-                    xrNodes[i].TryGetPosition (out newHandPosition);
-                    playerHand.transform.localPosition = newHandPosition;
+            // lefthand
+            if (xrNodes[i].nodeType == XRNode.LeftHand)
+            {
+                Vector3 newHandPosition;
+                xrNodes[i].TryGetPosition(out newHandPosition);
+                leftHand.transform.localPosition = newHandPosition;
 
-                    Quaternion newHandRotation;
-                    xrNodes[i].TryGetRotation (out newHandRotation);
-                    playerHand.transform.localRotation = newHandRotation;
+                Quaternion newHandRotation;
+                xrNodes[i].TryGetRotation(out newHandRotation);
+                leftHand.transform.localRotation = newHandRotation;
 
-                    InputDevices.GetDeviceAtXRNode (xrNodes[i].nodeType).TryGetFeatureValue (CommonUsages.trigger, out triggerPress);
+                InputDevices.GetDeviceAtXRNode(xrNodes[i].nodeType).TryGetFeatureValue(CommonUsages.trigger, out triggerPress);
+                InputDevices.GetDeviceAtXRNode(xrNodes[i].nodeType).TryGetFeatureValue(CommonUsages.grip, out grabPress);
 
-                    if (triggerPress > 0.6f) {
-                        triggering = true;
-                    } else {
-                        triggering = false;
-                    }
-
+                if (triggerPress > 0.6f)
+                {
+                    triggering = true;
+                }
+                else
+                {
+                    triggering = false;
                 }
 
-            } else {
+                if (grabPress > 0.6f)
+                {
+                    undo = true;
+                }
+                else
+                {
+                    undo = false;
+                }
+
+            }
+
+
+            // righthand
+            if (xrNodes[i].nodeType == XRNode.RightHand)
+            {
+                Vector3 newHandPosition;
+                xrNodes[i].TryGetPosition(out newHandPosition);
+                rightHand.transform.localPosition = newHandPosition;
+
+                Quaternion newHandRotation;
+                xrNodes[i].TryGetRotation(out newHandRotation);
+                rightHand.transform.localRotation = newHandRotation;
+
+                InputDevices.GetDeviceAtXRNode(xrNodes[i].nodeType).TryGetFeatureValue(CommonUsages.trigger, out triggerPressRight);
+                InputDevices.GetDeviceAtXRNode(xrNodes[i].nodeType).TryGetFeatureValue(CommonUsages.grip, out grabPressRight);
+
+                if (triggerPressRight > 0.6f)
+                {
+                    triggeringRight = true;
+                }
+                else
+                {
+                    triggeringRight = false;
+                }
+
+                if (grabPressRight > 0.6f)
+                {
+                    undoRight = true;
+                }
+                else
+                {
+                    undoRight = false;
+                }
+
+            }
+
+            //  }
+            else
+            {
 
             }
 
         }
 
-        if (triggering == true && lastTriggering == false) {
-            StartCreatingStrand ();
+        if (triggering == true && lastTriggering == false)
+        {
+            StartCreatingStrand(leftHand);
         }
-        if (lastTriggering == true && triggering == false) {
-            StopCreatingStrand ();
+        if (lastTriggering == true && triggering == false)
+        {
+            StopCreatingStrand();
+        }
+
+        if (undo == true && lastUndo == false)
+        {
+            Undo();
+        }
+        if (lastUndo == true && undo == false)
+        {
+
+        }
+
+        // right
+        if (triggeringRight == true && lastTriggeringRight == false)
+        {
+            StartCreatingStrand(rightHand);
+        }
+        if (lastTriggeringRight == true && triggeringRight == false)
+        {
+            StopCreatingStrand();
+        }
+
+        if (undoRight == true && lastUndoRight == false)
+        {
+            Undo();
+        }
+        if (lastUndoRight == true && undoRight == false)
+        {
+
         }
 
     }
     // Export hairdo to OBJ.
-    public void ExportOBJ () {
-        BakeHairdo ();
+    public void ExportOBJ()
+    {
+        hairStrands.RemoveAll(x => x == null);
+        strandMeshes = new List<Mesh>();
+        BakeHairdo();
     }
 
     // callback to initialize the hair creation process
-    void StartCreatingStrand () {
-        currentStrand = Instantiate (strandPrefab, playerHand.transform.position, playerHand.transform.rotation);
-        currentStrand.transform.parent = playerHand.transform;
-        Material randomStrandMaterial = currentHairType[Random.Range (0, currentHairType.Length)];
-        currentStrand.GetComponent<hairStrand> ().init (randomStrandMaterial);
+    void StartCreatingStrand(GameObject initiatingHand)
+    {
+        currentStrand = Instantiate(strandPrefab, initiatingHand.transform.position, initiatingHand.transform.rotation);
+        currentStrand.transform.parent = initiatingHand.transform;
+        Material randomStrandMaterial = currentHairType[Random.Range(0, currentHairType.Length)];
+        currentStrand.GetComponent<hairStrand>().init(randomStrandMaterial);
     }
 
     // callback once we release the trigger to create a strand and add it to the strand list
-    void StopCreatingStrand () {
-        currentStrand.transform.parent = null;
-        hairStrands.Add (currentStrand.GetComponent<TrailRenderer> ());
-        currentStrand = null;
+    void StopCreatingStrand()
+    {
+        if (currentStrand != null)
+        {
+            if (currentStrand.transform.parent != null)
+            {
+                currentStrand.transform.parent = null;
+            }
+
+            currentStrand.GetComponent<TrailRenderer>().emitting = false;
+            hairStrands.Add(currentStrand.GetComponent<TrailRenderer>());
+            currentStrand = null;
+        }
     }
 
     // change hair type
-    void ChangeHairType (Material[] newHairType) {
+    void ChangeHairType(Material[] newHairType)
+    {
         currentHairType = newHairType;
     }
 
     // Bake strands into meshes and sends the geometry into the exporter
-    void BakeHairdo () {
+    void BakeHairdo()
+    {
 
-        Mesh exportedHairdo = new Mesh ();
+        Mesh exportedHairdo = new Mesh();
 
-        for (int i = 0; i < hairStrands.Count; i++) {
-            Mesh bakedStrand = new Mesh ();
-            hairStrands[i].BakeMesh (bakedStrand, true);
-            strandMeshes.Add (bakedStrand);
+        for (int i = 0; i < hairStrands.Count; i++)
+        {
+            Mesh bakedStrand = new Mesh();
+            exportCamera.gameObject.transform.LookAt(hairStrands[i].transform);
+            hairStrands[i].BakeMesh(bakedStrand, exportCamera, true);
+            strandMeshes.Add(bakedStrand);
         }
 
         CombineInstance[] combine = new CombineInstance[strandMeshes.Count];
-        for (int i = 0; i < combine.Length; i++) {
+        for (int i = 0; i < combine.Length; i++)
+        {
             combine[i].mesh = strandMeshes[i];
         }
 
-        Mesh[] strandsArray = strandMeshes.ToArray ();
+        Mesh[] strandsArray = strandMeshes.ToArray();
 
-        exportedHairdo.CombineMeshes (combine, true, false);
+        exportedHairdo.CombineMeshes(combine, true, false);
         exportTarget.mesh = exportedHairdo;
 
-        MeshToFile (exportTarget, "hairdoExport.obj");
+        string timeText = System.DateTime.Now.ToString("hh.mm.ss");
+        MeshToFile(exportTarget, "hairdoExport " + timeText + ".obj");
     }
 
     // courtesy of KeliHlodversson @ https://wiki.unity3d.com/index.php/ObjExporter
-    static string MeshToString (MeshFilter mf) {
+    static string MeshToString(MeshFilter mf)
+    {
         Mesh m = mf.mesh;
-        Material[] mats = mf.GetComponent<Renderer> ().materials;
+        Material[] mats = mf.GetComponent<Renderer>().materials;
 
-        StringBuilder sb = new StringBuilder ();
+        StringBuilder sb = new StringBuilder();
 
-        sb.Append ("g ").Append (mf.name).Append ("\n");
-        foreach (Vector3 v in m.vertices) {
-            sb.Append (string.Format ("v {0} {1} {2}\n", v.x, v.y, v.z));
+        sb.Append("g ").Append(mf.name).Append("\n");
+        foreach (Vector3 v in m.vertices)
+        {
+            sb.Append(string.Format("v {0} {1} {2}\n", v.x, v.y, v.z));
         }
-        sb.Append ("\n");
-        foreach (Vector3 v in m.normals) {
-            sb.Append (string.Format ("vn {0} {1} {2}\n", v.x, v.y, v.z));
+        sb.Append("\n");
+        foreach (Vector3 v in m.normals)
+        {
+            sb.Append(string.Format("vn {0} {1} {2}\n", v.x, v.y, v.z));
         }
-        sb.Append ("\n");
-        foreach (Vector3 v in m.uv) {
-            sb.Append (string.Format ("vt {0} {1}\n", v.x, v.y));
+        sb.Append("\n");
+        foreach (Vector3 v in m.uv)
+        {
+            sb.Append(string.Format("vt {0} {1}\n", v.x, v.y));
         }
-        for (int material = 0; material < m.subMeshCount; material++) {
-            sb.Append ("\n");
-            sb.Append ("usemtl ").Append ("hairMaterial").Append ("\n");
-            sb.Append ("usemap ").Append ("hairMaterial").Append ("\n");
+        for (int material = 0; material < m.subMeshCount; material++)
+        {
+            sb.Append("\n");
+            sb.Append("usemtl ").Append("hairMaterial").Append("\n");
+            sb.Append("usemap ").Append("hairMaterial").Append("\n");
 
-            int[] triangles = m.GetTriangles (material);
-            for (int i = 0; i < triangles.Length; i += 3) {
-                sb.Append (string.Format ("f {0}/{0}/{0} {1}/{1}/{1} {2}/{2}/{2}\n",
+            int[] triangles = m.GetTriangles(material);
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                sb.Append(string.Format("f {0}/{0}/{0} {1}/{1}/{1} {2}/{2}/{2}\n",
                     triangles[i] + 1, triangles[i + 1] + 1, triangles[i + 2] + 1));
             }
         }
-        return sb.ToString ();
+        return sb.ToString();
     }
 
     // courtesy of KeliHlodversson @ https://wiki.unity3d.com/index.php/ObjExporter
-    static void MeshToFile (MeshFilter mf, string filename) {
-        using (StreamWriter sw = new StreamWriter (filename)) {
-            sw.Write (MeshToString (mf));
+    static void MeshToFile(MeshFilter mf, string filename)
+    {
+        using (StreamWriter sw = new StreamWriter(filename))
+        {
+            sw.Write(MeshToString(mf));
         }
     }
 }
