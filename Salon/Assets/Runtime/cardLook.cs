ca329baf5764d54574521307ffacfabfd0a4c2e2
Alexis <alexis.morin.mail@gmail.com>
2019-05-22 16:42:00 -0400

    new shader, started work on hair strand auto alignment

---
+++ Salon/Assets/Runtime/cardLook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardLook : MonoBehaviour {
+
+    Transform lookTarget;
+    // Start is called before the first frame update
+    void Start () {
+        lookTarget = GameObject.Find ("cardLookRoot").transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt (lookTarget);
+    }
+}
